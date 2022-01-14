d02656ba3082e50ea6854996d1e96e7bad966dac
JohnFCreep <79384133+JohnFCreep@users.noreply.github.com>
2022-01-14 13:20:29 +0100

    Update database.php

--- database.php
+++ database.php
@@ -11,8 +11,14 @@
   $accresult = mysqli_query($db, $sql);
   $accrow = mysqli_fetch_assoc($accresult);
   $account = mysqli_num_rows($accresult);
-   
-  $DB_DATABASE2 = "phase_g_words";
-  $DB_UNAME = $_COOKIE["login_user"];
-  $dbw = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, $DB_DATABASE2);
+
+  $sql = "SELECT * FROM accounts WHERE username = '$usernametest'";
+  $adminresult = mysqli_query($db, $sql);
+  $adminrow = mysqli_fetch_assoc($adminresult);
+  
+  if ($adminrow['id'] != "1") {
+    $DB_DATABASE2 = "phase_g_words";
+    $DB_UNAME = $_COOKIE["login_user"];
+    $dbw = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, $DB_DATABASE2);
+  }
 ?>
