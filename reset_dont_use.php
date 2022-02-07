06025821b15c1ad7e8331ada4aa54c6037535702
JohnFCreep <79384133+JohnFCreep@users.noreply.github.com>
2022-02-07 03:26:07 +0100

    Nur zu test/demo Zecken benutzen!
    Diese Datei sollte nicht Zugängig sein, da sie den kompletten Fortschritt zurücksetzt. Dies sollte nur bei Demo-Benutzern möglich sein

--- reset_dont_use.php
+++ reset_dont_use.php
@@ -1,6 +1,5 @@
 <?php
   include './database.php';
-  
   $sql = "UPDATE words_$usernametest SET level='0', backlevel='0', date='0', backdate='0'";
   mysqli_query($dbw, $sql);
   header("location: ./index.php");
