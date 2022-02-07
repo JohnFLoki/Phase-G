e39125291dfb5b0924125d60e356da37afc846a4
JohnFCreep <79384133+JohnFCreep@users.noreply.github.com>
2022-02-07 02:08:16 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -237,7 +237,7 @@ echo '
           ';
           if($accrow['github'] == "on"){
             echo '
-            <a class="backgitreport rebackgitreport" href="https://github.com/JohnFCreep/Phase-G/issues/new?assignees=&labels=&template=vokabel-fehlerhaft.md&title=Vokabel fehlerhaft TtG&body=Name: ' . $row["name"] . ' %40 Link: ' . $row["link"] . '" target="_blank">Sollte das Video nicht laden, klicke hier (Github).</a>
+            <a class="backgitreport rebackgitreport" href="https://github.com/JohnFCreep/Phase-G/issues/new?assignees=&labels=&template=vokabel-fehlerhaft.md&title=Vokabel fehlerhaft TtG&body=Link:' . $lastrow['link'] . '" target="_blank">Sollte das Video nicht laden, klicke hier (Github).</a>
             ';
           }
         echo '
@@ -378,7 +378,7 @@ echo '
             ';
             if($accrow['github'] == "on"){
               echo '
-              <a class="backgitreport" href="https://github.com/JohnFCreep/Phase-G/issues/new?assignees=&labels=&template=vokabel-fehlerhaft.md&title=Vokabel fehlerhaft TtG&body=Name: ' . $row["name"] . ' %40 Link: ' . $row["link"] . '" target="_blank">Sollte das Wort komisch sein, klicke hier (Github).</a>
+              <a class="backgitreport" href="https://github.com/JohnFCreep/Phase-G/issues/new?assignees=&labels=&template=vokabel-fehlerhaft.md&title=Vokabel fehlerhaft TtG&body=Name: ' . $backrow["name"] . ' %40 Link: ' . $backrow["link"] . '" target="_blank">Sollte das Wort komisch sein, klicke hier (Github).</a>
               ';
             }
           echo '
